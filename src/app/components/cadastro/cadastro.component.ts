1bc17a7b87eb6169e0f3b92cc83169debb9bdc17
ConhecedorX~ <Lucastation2@gmail.com>
2024-11-18 13:05:38 -0300

    .

--- src/app/components/cadastro/cadastro.component.ts
+++ src/app/components/cadastro/cadastro.component.ts
@@ -1,14 +1,30 @@
 import { Component } from '@angular/core';
 import { TesteComponent } from '../teste/teste.component';
 import { FooterComponent } from '../footer/footer.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-cadastro',
   standalone: true,
-  imports: [ TesteComponent, FooterComponent],
+  imports: [ TesteComponent, FooterComponent, CommonModule],
   templateUrl: './cadastro.component.html',
   styleUrl: './cadastro.component.css'
 })
 export class CadastroComponent {
+  imageSrc: string | ArrayBuffer | null = null;
 
+  onImageSelected(event: Event): void {
+    const input = event.target as HTMLInputElement;
+
+    if (input.files && input.files[0]) {
+      const file = input.files[0];
+      const reader = new FileReader();
+
+      reader.onload = () => {
+        this.imageSrc = reader.result;
+      };
+
+      reader.readAsDataURL(file);
+    }
+  }
 }
